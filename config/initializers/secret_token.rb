71d675c4845faf200a0b7a581ef5a87f7f99056d
macieekh <maciej.harazinski@gmail.com>
2013-08-18 18:28:36 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carminder::Application.config.secret_token = 'c1410789edf4d42fd6a6387273dbdd5c10d5c4c0c64295479a8e38bef87db91907ec4cf1558542678415d8087cb24397359849503b4796bb35e623fbbc72f421'
